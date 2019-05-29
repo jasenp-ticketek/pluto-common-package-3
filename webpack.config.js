c2d36f6aad9f23374b3cf2bf5e74d348e36e4853
TT\jasenp <jasenp@ticketek.com.au>
2019-05-29 11:13:30 +1000

    build into umd

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   mode: "production",
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd"
   },
   module: {
     rules: [
